16d791aeb52580e3fd12f86e42a8c7151341124b
charanya312 <162348601+charanya312@users.noreply.github.com>
2025-03-09 23:19:48 +0530

    Add files via upload

---
+++ lesson1/1a.js
@@ -0,0 +1,9 @@
+alert('Good morning!');
+alert('Charanya');
+10+5
+20-5
+document.body.innerHtml='Good morning!';
+document.body.innerHtml='charanya';
+10+8+20
+100-20-50+200
+document.body.innerHtml = '';
